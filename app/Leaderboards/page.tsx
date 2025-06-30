0cb78b640956a528deb1510a43f20742d1f8a9d1
Flashl3opard <sheoreyyash@gmail.com>
2025-06-30 14:04:54 +0530

    Added Leaderboards.tsx for the sake of deployement

---
+++ app/Leaderboards/page.tsx
@@ -0,0 +1,13 @@
+// app/Leaderboards/page.tsx
+
+import React from "react";
+
+const LeaderboardsPage = () => {
+  return (
+    <div>
+      <h1>Leaderboards</h1>
+    </div>
+  );
+};
+
+export default LeaderboardsPage;
