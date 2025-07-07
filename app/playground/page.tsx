7c0e82dd487650e3ccc986142dcac6b76ce5ebc2
Mrinal-Agrawal21 <agrawalmrinal21@gmail.com>
2025-07-07 16:56:14 +0530

    Designal changes in playground page.tsx

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -4,7 +4,7 @@ import Lead from "../components/Lead";
 
 const page = () => {
   return (
-    <div className="flex flex-row justify-end gap-10 p-5">
+    <div className="flex flex-col justify-around gap-10 p-5">
       <SoundBoard />
       <Lead />
     </div>
