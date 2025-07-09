ca94c7bf438cd9db963825c67e7b17cd67a90005
Flashl3opard <sheoreyyash@gmail.com>
2025-07-09 16:11:31 +0530

    UI fixes, completed issue #61

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,26 +1,30 @@
 import React from "react";
 import SoundBoard from "../components/SoundBoard";
 import Lead from "../components/Lead";
+import Navbar from "../components/Navbar";
 
 const page = () => {
   return (
-    <main className="min-h-screen  dark:bg-gray-900 p-6">
-      <div className="max-w-6xl mx-auto flex flex-col lg:flex-row gap-8 items-stretch">
-        {/* SoundBoard Section */}
-        <div className="lg:w-2/3 w-full">
-          <div className="bg-white dark:bg-gray-800 p-4 rounded-2xl shadow-md h-full">
-            <SoundBoard />
+    <>
+      <Navbar />
+      <main className="min-h-screen  dark:bg-[#020612] p-6">
+        <div className="max-w-6xl mx-auto flex flex-col lg:flex-row gap-8 items-stretch">
+          {/* SoundBoard Section */}
+          <div className="lg:w-2/3 w-full">
+            <div className="bg-white dark:bg-gray-800 p-4 rounded-2xl shadow-md h-full">
+              <SoundBoard />
+            </div>
           </div>
-        </div>
 
-        {/* Lead Section */}
-        <div className="lg:w-1/3 w-full">
-          <div className="bg-white dark:bg-gray-800 p-4 rounded-2xl shadow-md h-full">
-            <Lead />
+          {/* Lead Section */}
+          <div className="lg:w-1/3 w-full">
+            <div className="bg-white dark:bg-gray-800 p-4 rounded-2xl shadow-md h-full">
+              <Lead />
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 };
 
