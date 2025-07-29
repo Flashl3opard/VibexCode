ed4f9784f070eb53e43236670885dd07c0ea589f
vanshikaaa <133578313+Vanshikaxxa@users.noreply.github.com>
2025-07-29 19:53:30 +0530

    Added files for clans

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,10 +1,10 @@
-import { Client, Account } from "appwrite";
+import { Client, Account, Databases, ID, Query } from "appwrite";
 
-// ✅ Appwrite Client Setup (without JWT logic)
 const client = new Client()
   .setEndpoint(process.env.NEXT_PUBLIC_API_ENDPOINT as string)
   .setProject(process.env.NEXT_PUBLIC_PROJECT_ID as string);
 
 const account = new Account(client);
+const databases = new Databases(client);
 
-export { client, account };
+export { client, account, databases, ID, Query };
